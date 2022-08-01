ce6b21fb2b5361fe07aa85929e2b454fa83333a1
zako <velizar9209@gmail.com>
2022-08-01 23:02:02 +0300

    Projects set up

---
+++ CSharpNet-Web-System/CSharpNet-Web-System.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSharpNet_Web_System.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
