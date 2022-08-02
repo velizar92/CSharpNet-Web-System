55ef5610742a042c48b61b7be9ab59c8852e61cb
zako <velizar9209@gmail.com>
2022-08-02 22:48:32 +0300

    Making of ResourceService - done

--- CSharpNet-Web-System/CSharpNet-Web-System.Services/Resources/ResourceService.cs
+++ CSharpNet-Web-System/CSharpNet-Web-System.Services/Resources/ResourceService.cs
@@ -1,11 +1,54 @@
 ﻿namespace CSharpNet_Web_System.Services.Resources
-{
-    using System;
+{  
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
+    using CSharpNet_Web_System.Data;
+    using Microsoft.EntityFrameworkCore;
+
     public class ResourceService : IResourceService
     {
+        private readonly CSharpNetWebDbContext _dbContext;
+
+        public ResourceService(CSharpNetWebDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public bool CheckIfResourceTypeExists(int resourceTypeId)
+        {
+            if (_dbContext.ResourceTypes.Any(r => r.Id == resourceTypeId) == false)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public async Task<ResultServiceModel> DeleteResource(int resourceId)
+        {
+            var resource = await _dbContext.Resources.FirstOrDefaultAsync(r => r.Id == resourceId);
+
+            if (resource == null)
+            {
+                return new ResultServiceModel(false, "Invalid resource id.");
+            }
+
+            _dbContext.Resources.Remove(resource);
+            await _dbContext.SaveChangesAsync();
+
+            return new ResultServiceModel(true, "OK");
+        }
+
+
+        public async Task<IEnumerable<string>> GetAllResourceTypes()
+        {
+           var allResourceTypes = await _dbContext.ResourceTypes
+                                             .Select(c => c.Name)
+                                             .Distinct()
+                                             .ToListAsync();
+
+            return allResourceTypes;
+        }
     }
 }
--- CSharpNet-Web-System/CSharpNet-Web-System.Services/Tutorials/Models/TutorialDetailsServiceModel.cs
+++ CSharpNet-Web-System/CSharpNet-Web-System.Services/Tutorials/Models/TutorialDetailsServiceModel.cs
@@ -1,13 +1,9 @@
 ﻿namespace CSharpNet_Web_System.Services.Tutorials.Models
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     public class TutorialDetailsServiceModel
-    {  public int Id { get; set; }
-
+    {
+        public int Id { get; set; }
         public int CourseId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
