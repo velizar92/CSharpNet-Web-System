964ee0901a3a0338fa4be7e642078fb302706ec0
zako <velizar9209@gmail.com>
2022-08-03 21:24:59 +0300

    Making of UsersController - done

---
+++ CSharpNet-Web-System/CSharpNet-Web-System.Web/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,114 @@
+﻿namespace CSharpNet_Web_System.Web.Areas.Admin.Controllers
+{
+    using CSharpNet_Web_System.Models.Models;
+    using CSharpNet_Web_System.Services.Storage;
+    using CSharpNet_Web_System.Web.Areas.Admin.Models.User;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using System.Security.Claims;
+
+    public class UsersController : AdminController
+    {
+
+        private readonly UserManager<User> _userManager;
+        private readonly IStorageService _fileStorageService;
+
+        public UsersController(        
+           UserManager<User> userManager,
+           IStorageService fileStorageService)
+        {
+            _userManager = userManager;
+            _fileStorageService = fileStorageService;
+        }
+
+
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateUser(UserFormModel userFormModel)
+        {
+            var user = GetUser(userFormModel.FirstName, userFormModel.LastName,
+                                userFormModel.Email, userFormModel.ProfileImage.FileName);
+
+            await _userManager.CreateAsync(user, userFormModel.Password);
+            await _userManager.AddToRoleAsync(user, "Learner");
+            await _userManager.AddClaimAsync(user, new Claim("ProfileImageUrl", user.ProfileImageUrl));
+
+            await _fileStorageService.SaveFile(@"\assets\images\users", userFormModel.ProfileImage);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> EditUser(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+
+            return View(new UserFormModel
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+            });
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditUser(UserFormModel userFormModel)
+        {
+            var user = GetUser(userFormModel.FirstName, userFormModel.LastName,
+                                userFormModel.Email, userFormModel.ProfileImage.FileName);
+
+            await _userManager.UpdateAsync(user);
+
+            await _fileStorageService.SaveFile(@"\assets\images\users", userFormModel.ProfileImage);          
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteUser(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+
+            await _userManager.DeleteAsync(user);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> AllUsers()
+        {
+            var users = await _userManager.Users.ToListAsync();
+          
+            return View(users);
+        }
+
+
+        private User GetUser(string firstName, string lastName,
+           string email, string profileImageUrl)
+        {
+            User user = new()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                UserName = email,
+                NormalizedUserName = email.ToUpper(),
+                Email = email,
+                ProfileImageUrl = profileImageUrl,
+            };
+
+            return user;
+        }
+    }
+}
---
+++ CSharpNet-Web-System/CSharpNet-Web-System.Web/Areas/Admin/Models/User/UserFormModel.cs
@@ -0,0 +1,36 @@
+﻿namespace CSharpNet_Web_System.Web.Areas.Admin.Models.User
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class UserFormModel
+    {
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Profile Image")]
+        public IFormFile ProfileImage { get; set; }
+    }
+}
