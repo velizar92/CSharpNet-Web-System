708347726aac0975b4028134d1a902d0fcaab0c3
zako <velizar9209@gmail.com>
2022-08-03 14:57:38 +0300

    Creating of CommentsController (Admin area) - done

---
+++ CSharpNet-Web-System/CSharpNet-Web-System.Web/Areas/Admin/Controllers/CommentsController.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharpNet_Web_System.Web.Areas.Admin.Controllers
+{
+    using CSharpNet_Web_System.Services.Comments;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CommentsController : AdminController
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentsController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+
+        [HttpGet]      
+        public async Task<IActionResult> DeleteComment(int commentId)
+        {
+            var resultSeviceModel = await _commentService.DeleteComment(commentId);
+
+            if (resultSeviceModel.Result == false)
+            {
+                return BadRequest(resultSeviceModel.Message);
+            }
+
+            return View();
+        }
+
+       
+    }
+}
