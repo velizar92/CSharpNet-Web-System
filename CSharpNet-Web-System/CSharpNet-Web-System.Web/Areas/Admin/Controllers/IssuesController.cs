e392640e72a6d513720ff6e923ae82a0677f1627
zako <velizar9209@gmail.com>
2022-08-03 15:27:25 +0300

    Making of IssueController (Admin area) - done

---
+++ CSharpNet-Web-System/CSharpNet-Web-System.Web/Areas/Admin/Controllers/IssuesController.cs
@@ -0,0 +1,37 @@
+﻿namespace CSharpNet_Web_System.Web.Areas.Admin.Controllers
+{
+    using CSharpNet_Web_System.Services.Issues;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class IssuesController : AdminController
+    {
+        private readonly IIssueService _issueService;
+
+        public IssuesController(IIssueService issueService)
+        {
+            _issueService = issueService;
+        }
+
+
+        public async Task<IActionResult> FixIssue(int issueId, int tutorialId)
+        {
+            var resultServiceModel = await _issueService.DeleteIssue(issueId);
+
+            if (resultServiceModel.Result == false)
+            {
+                return BadRequest(resultServiceModel.Message);
+            }
+
+            return RedirectToAction(nameof(MyIssues), new { tutorialId });
+        }
+
+
+        public async Task<IActionResult> MyIssues()
+        {
+            var allReportedIssues = await _issueService.GetAllReportedIssues();
+
+            return View(allReportedIssues);
+        }
+
+    }
+}
