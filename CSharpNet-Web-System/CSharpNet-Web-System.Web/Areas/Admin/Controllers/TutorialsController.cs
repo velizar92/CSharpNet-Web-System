d88ab73d9acf785665fe1a3fc3abb98e2795c04f
zako <velizar9209@gmail.com>
2022-08-03 14:14:49 +0300

    Making of TutorialsController (Admin area)

---
+++ CSharpNet-Web-System/CSharpNet-Web-System.Web/Areas/Admin/Controllers/TutorialsController.cs
@@ -0,0 +1,156 @@
+﻿namespace CSharpNet_Web_System.Web.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using CSharpNet_Web_System.Services.Storage;
+    using CSharpNet_Web_System.Services.Tutorials;
+    using CSharpNet_Web_System.Web.Areas.Admin.Models.Tutorial;
+    using CSharpNet_Web_System.Models.Models;
+
+    public class TutorialsController : AdminController
+    {
+        private readonly ITutorialService _tutorialService;
+        private readonly IStorageService _fileStorageService;
+
+        public TutorialsController(ITutorialService tutorialService, IStorageService fileStorageService)
+        {
+            _tutorialService = tutorialService;
+            _fileStorageService = fileStorageService;
+        }
+
+        [HttpGet]
+        public IActionResult CreateTutorial()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateTutorial(int courseId, TutorialFormModel tutorialFormModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var resources = GetResources(tutorialFormModel.Files);
+
+            var resultServiceModel = await _tutorialService.AddTutorialToCourse
+                                                (courseId, tutorialFormModel.Title, tutorialFormModel.Description, resources);
+
+            if (resultServiceModel.Result == false)
+            {
+                return BadRequest(resultServiceModel.Message);
+            }
+
+            await _fileStorageService.SaveFiles(@"\assets\resources", tutorialFormModel.Files);
+            return RedirectToAction("Details", "CourseDetails", new { courseId });
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> EditTutorial(int id)
+        {
+            var tutorial = await _tutorialService.GetTutorialById(id);
+
+            if (tutorial == null)
+            {
+                return NotFound();
+            }
+
+            TutorialFormModel lectureFormModel = new TutorialFormModel()
+            {
+                Id = tutorial.Id,
+                CourseId = tutorial.CourseId,
+                Title = tutorial.Title,
+                Description = tutorial.Description,
+                Resources = tutorial.Resources.ToArray(),
+            };
+
+            return View(lectureFormModel);
+        }
+
+
+        [HttpPost, DisableRequestSizeLimit]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditTutorial(int id, TutorialFormModel tutorialFormModel)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View(tutorialFormModel);
+            }
+
+            var tutorial = await _tutorialService.GetTutorialById(id);
+
+            if (tutorial == null)
+            {
+                return NotFound();
+            }
+
+            var resources = GetResources(tutorialFormModel.Files);
+            var resultServiceModel = await _tutorialService.EditTutorial(id, tutorialFormModel.Title, tutorialFormModel.Description, resources);
+
+            if (resultServiceModel.Result == false)
+            {
+                return BadRequest(resultServiceModel.Message);
+            }
+
+            await _fileStorageService.SaveFiles(@"\assets\resources", tutorialFormModel.Files);
+            return RedirectToAction(nameof(Details), new { id });
+        }
+
+
+
+        [HttpGet]
+         public async Task<IActionResult> DeleteTutorial(int id, int courseId)
+        {
+            var resultServiceModel = await _tutorialService.DeleteTutorial(id);
+
+            if (resultServiceModel.Result == true)
+            {            
+                return RedirectToAction("Details", new { courseId });
+            }
+
+            return BadRequest();
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {         
+            var tutorialDetails = await _tutorialService.GetTutorialDetails(id);
+       
+             return View(tutorialDetails);                 
+        }
+
+
+
+
+        private List<Resource> GetResources(IEnumerable<IFormFile> resourceFiles)
+        {
+            List<Resource> resources = new List<Resource>();
+            int tempResourceId = 1;
+
+            foreach (var resourceFile in resourceFiles)
+            {
+                if (resourceFile.ContentType == "pdf")
+                {
+                    tempResourceId = 3;
+                }
+                else if (resourceFile.ContentType == "mp4")
+                {
+                    tempResourceId = 2;
+                }
+                else
+                {
+                    tempResourceId = 1;
+                }
+
+                resources.Add(new Resource { Name = resourceFile.FileName, ResourceTypeId = tempResourceId });
+            }
+
+            return resources;
+        }
+    }
+}
---
+++ CSharpNet-Web-System/CSharpNet-Web-System.Web/Areas/Admin/Models/Tutorial/TutorialFormModel.cs
@@ -0,0 +1,34 @@
+﻿using CSharpNet_Web_System.Models.Models;
+using System.ComponentModel.DataAnnotations;
+
+using static CSharpNet_Web_System.Models.DataConstants.Tutorial;
+
+namespace CSharpNet_Web_System.Web.Areas.Admin.Models.Tutorial
+{
+    public class TutorialFormModel
+    {
+        public int Id { get; set; }
+
+        public int CourseId { get; set; }
+
+
+        [Required(ErrorMessage = "Field {0} is required.")]
+        [StringLength(
+            TutorialTitleMaxLength,
+            MinimumLength = TutorialTitleMinLength,
+            ErrorMessage = "The field Content must be a string with a minimum length of {2} and maximum length of {1}.")]
+        public string Title { get; set; }
+
+
+        [Required(ErrorMessage = "Field {0} is required.")]
+        [StringLength(
+            TutorialDescriptionMaxLength,
+            MinimumLength = TutorialDescriptionMinLength,
+            ErrorMessage = "The field Content must be a string with a minimum length of {2} and maximum length of {1}.")]
+        public string Description { get; set; }
+
+        public Resource[]? Resources { get; set; }
+
+        public IEnumerable<IFormFile> Files { get; set; }
+    }
+}
