bb74e277dbdd1e0055f7c8c4fdc98aa0ec7d5933
zako <velizar9209@gmail.com>
2022-08-03 14:15:31 +0300

    Making of TutorialsController (not Admin area)

---
+++ CSharpNet-Web-System/CSharpNet-Web-System.Web/Controllers/TutorialsController.cs
@@ -0,0 +1,27 @@
+﻿namespace CSharpNet_Web_System.Web.Controllers
+{
+    using CSharpNet_Web_System.Services.Tutorials;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class TutorialsController : Controller
+    {
+        private readonly ITutorialService _tutorialService;       
+
+        public TutorialsController(ITutorialService tutorialService)
+        {
+            _tutorialService = tutorialService;
+        }
+
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Details(int id)
+        {
+            var tutorialDetails = await _tutorialService.GetTutorialDetails(id);
+
+            return View(tutorialDetails);
+        }
+
+    }
+}
