11c30b7af5a800110d19584a20e2a69d8048b38a
zako <velizar9209@gmail.com>
2022-08-03 19:25:33 +0300

    Making of PostsController (Admin area) - done

---
+++ CSharpNet-Web-System/CSharpNet-Web-System.Web/Areas/Admin/Models/Post/PostFormModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharpNet_Web_System.Web.Areas.Admin.Models.Post
+{
+    public class PostFormModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
