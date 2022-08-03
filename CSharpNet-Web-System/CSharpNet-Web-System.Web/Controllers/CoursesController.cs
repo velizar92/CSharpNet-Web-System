ff3b6277bc8b1b3b93578c12dafe062a2a8c88da
zako <velizar9209@gmail.com>
2022-08-03 13:04:08 +0300

    Adding of CoursesController (Not Admin Area) with AllCourses/CourseDetails actions

---
+++ CSharpNet-Web-System/CSharpNet-Web-System.Web/Controllers/CoursesController.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharpNet_Web_System.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+    using CSharpNet_Web_System.Services.Courses;
+
+    public class CoursesController : Controller
+    {
+
+        private readonly ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> AllCourses()
+        {
+            var allCourses = await _courseService.GetAllCourses();
+            return View(allCourses);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> CourseDetails(int courseId)
+        {
+            var courseDetails = await _courseService.GetCourseDetails(courseId);
+            return View(courseDetails);
+        }
+    }
+}
