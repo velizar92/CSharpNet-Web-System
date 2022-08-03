971ce66aae7ef6d752af8b6b699ba3f21c3e5ce0
zako <velizar9209@gmail.com>
2022-08-03 14:25:14 +0300

    Making of ResourcesController (Admin area) - done

---
+++ CSharpNet-Web-System/CSharpNet-Web-System.Web/Areas/Admin/Controllers/ResourcesController.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharpNet_Web_System.Web.Areas.Admin.Controllers
+{
+    using CSharpNet_Web_System.Services.Resources;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ResourcesController : AdminController
+    {
+        private readonly IResourceService _resourceService;
+       
+
+        public ResourcesController(IResourceService resourceService)
+        {
+            _resourceService = resourceService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteResource(int resourceId, int tutorialId)
+        {    
+            var resultServiceModel = await _resourceService.DeleteResource(resourceId);
+
+            if (resultServiceModel.Result == false)
+            {
+                return BadRequest(resultServiceModel.Message);
+            }
+
+            return RedirectToAction("Details", "Tutorials", new { tutorialId });
+        }
+    }
+}
