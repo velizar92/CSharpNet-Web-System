8a2aa2ab01195965b0cc4bcd67e517ebac6bd1e4
zako <velizar9209@gmail.com>
2022-08-03 14:56:57 +0300

    Add extension to infrastructure project

---
+++ CSharpNet-Web-System/CSharpNet-Web-System.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharpNet_Web_System.Infrastructure.Extensions
+{
+    using System.Security.Claims;
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {          
+            return user.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole("Admin");
+        }
+    }
+}
