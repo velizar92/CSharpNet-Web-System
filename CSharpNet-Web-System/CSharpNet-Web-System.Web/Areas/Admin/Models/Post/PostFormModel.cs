e515613023a5abdde6464c1bd2c484b504ea55e7
zako <velizar9209@gmail.com>
2022-08-06 21:14:44 +0300

    Deletion of not needed Post stuff

--- CSharpNet-Web-System/CSharpNet-Web-System.Web/Areas/Admin/Models/Post/PostFormModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CSharpNet_Web_System.Web.Areas.Admin.Models.Post
-{
-    public class PostFormModel
-    {
-        public string Title { get; set; }
-        public string Content { get; set; }
-    }
-}
