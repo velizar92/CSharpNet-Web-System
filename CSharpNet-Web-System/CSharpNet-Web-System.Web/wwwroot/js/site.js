0b4edc6666a9ed74eb0689bc0173d4ca15be3827
zako <velizar9209@gmail.com>
2022-09-25 19:12:17 +0300

    Adding of preload page animation + new background

--- CSharpNet-Web-System/CSharpNet-Web-System.Web/wwwroot/js/site.js
+++ CSharpNet-Web-System/CSharpNet-Web-System.Web/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var loader = document.querySelector('.loader');
+
+window.addEventListener("load", function () {
+    loader.style.display = "none";
+})
