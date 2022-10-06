f4d6133f65342e6b4b878edcecd8ca27e82e0422
zako <velizar9209@gmail.com>
2022-10-06 17:25:20 +0300

    Making of ViewComponent for HeaderMenu + course Ids

---
+++ CSharpNet-Web-System/CSharpNet-Web-System.Web/ViewComponents/HeaderMenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using CSharpNet_Web_System.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpNet_Web_System.Web.ViewComponents
+{
+    [ViewComponent(Name = "HeaderMenu")]
+    public class HeaderMenuViewComponent : ViewComponent
+    {
+        private readonly CSharpNetWebDbContext _context;
+
+        public HeaderMenuViewComponent(CSharpNetWebDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("Index", _context.Courses.ToList());
+        }
+    }
+}
