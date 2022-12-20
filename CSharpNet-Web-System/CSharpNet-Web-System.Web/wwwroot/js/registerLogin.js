d770d27e80bb20fafbfd02b876a506989f196997
Velizar Gerasimov <velizar9209@gmail.com>
2022-12-20 16:16:35 +0200

    Including of refisterLogin.js - CSWS-113"

---
+++ CSharpNet-Web-System/CSharpNet-Web-System.Web/wwwroot/js/registerLogin.js
@@ -0,0 +1,9 @@
+﻿let input = document.getElementById("inputBox");
+let imageName = document.getElementById("imageName")
+
+input.addEventListener("change", () => {
+    let inputImage = document.querySelector("input[type=file]").files[0];
+
+    imageName.innerText = inputImage.name;
+})
+
